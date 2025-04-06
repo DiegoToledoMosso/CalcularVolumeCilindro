9eac77691d533cb0c28c2b1e45c697b1107b0b06
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-06 02:40:23 -0300

    Refatoração

--- CalcularVolumeCilindro.ConsoleApp1/Program.cs
+++ CalcularVolumeCilindro.ConsoleApp1/Program.cs
@@ -1,10 +1,56 @@
 ﻿namespace CalcularVolumeCilindro.ConsoleApp1
 {
+
+  
     internal class Program
+
+    
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while (true)
+            {
+                int r, h;
+
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Olá, aqui iremos calcular o volume do seu cilindro");
+                Console.WriteLine("--------------------------------------------------");
+
+                Console.WriteLine("Para começar, irei precisar das medidas do seu cilindro:");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("digite a medida do raio do cilindro");
+
+                //var r = Convert.ToInt32(Console.ReadLine());
+                r = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("digite a medida da altura do cilindro ");
+
+                //var h = Convert.ToDecimal(Console.ReadLine());
+                h = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("----------------------------------------");
+
+
+
+
+                var volume = Math.PI * Math.Pow(r, 2) * h;
+
+                
+                Console.WriteLine($"Volume do cilindro é {volume:0.##}");
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+               
+
+
+            }
+
         }
     }
 }
